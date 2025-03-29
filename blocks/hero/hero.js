19bfe758c7a617692d6389955c04ae2cb3f98725
Daniel Hernandez <daniel.hernandez@wearepop.com>
2025-03-28 18:00:43 -0600

    updating hero title field to use title comp

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,3 +1,6 @@
 export default function decorate(block) {
-  block.children[1].classList.add('kd-layout__content-container');
+  block.children[0].classList.add('hero__logo-container');
+  block.children[1].classList.add('hero__background-container');
+  block.children[2].classList.add('kd-layout__content-container');
+  block.children[3].classList.add('kd-layout__content-container');
 }
